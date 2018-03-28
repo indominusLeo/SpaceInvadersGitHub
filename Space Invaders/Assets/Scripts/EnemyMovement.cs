7c95afa75f2d4fa33b3a5a45e372f7686ceaf817
indominusLeo <leovca2207@gmail.com>
2018-03-28 01:47:22 -0400

    [ADD] Medium Homework

---
+++ Space Invaders/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float startTime;
+
+    private float velocity = 0.5f;
+
+    private Transform enemyTransform;
+
+    private int count;
+
+    private float deltaTime = 1.0f;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        count = 0;
+        enemyTransform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        InvokeRepeating("enemyMovement", startTime, deltaTime);
+
+    }
+	
+	void enemyMovement () {
+
+        count++;
+        anim.SetTrigger("Move");
+
+        if (count == 18)
+        {
+            count = 0;
+            velocity = -velocity;
+            enemyTransform.position += Vector3.down * 0.5f;
+            enemyTransform.position += Vector3.right * velocity;
+            count++;
+            CancelInvoke();
+            InvokeRepeating("enemyMovement", 1, deltaTime - 0.2f);
+        } else
+        {
+            enemyTransform.position += Vector3.right * velocity;
+        }
+	}
+}
